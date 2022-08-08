9fdfba6b990233a17e8f4db0c959d2368616767d
Aymeric Goube <aymericgoube@hotmail.fr>
2022-08-08 18:09:46 +0200

    modifying background & player character

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
+const myObstacles = [];
 
 document.getElementById("buttonStart").addEventListener("click", () => {
   road.draw();
   car.draw();
 });
+
+const roadImg = new Image();
+roadImg.src = "../images/space.jpeg";
 
 class Road {
   constructor(width, height, x, y) {
@@ -12,31 +16,29 @@ class Road {
     this.height = height;
     this.x = x;
     this.y = y;
+    this.frames = 0;
   }
 
   draw() {
-    const roadImg = new Image();
-    roadImg.src = "../images/road.png";
-    roadImg.onload = () => {
-      ctx.drawImage(roadImg, this.width, this.height, this.x, this.y);
-    };
+    ctx.drawImage(roadImg, this.width, this.height, this.x, this.y);
+  }
+  clear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 }
-const road = new Road(0, 0, 500, 700);
+const road = new Road(0, 0, 800, 600);
+const carImg = new Image();
+carImg.src = "../images/spacecraft.png";
 
 class Car {
   constructor() {
     this.width = 75;
     this.height = 150;
-    this.x = 240;
-    this.y = 520;
+    this.x = 300;
+    this.y = 420;
   }
   draw() {
-    const carImg = new Image();
-    carImg.src = "../images/car.png";
-    carImg.onload = () => {
-      ctx.drawImage(carImg, this.x, this.y, this.width, this.height);
-    };
+    ctx.drawImage(carImg, this.x, this.y, this.width, this.height);
   }
   moveUp() {
     car.y -= 25;
@@ -50,36 +52,77 @@ class Car {
   moveRight() {
     this.x += 25;
   }
-  clear() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  }
 }
 const car = new Car();
 function updateCanvas() {
+  road.clear();
   road.draw();
   car.draw();
+  updateObstacles();
   //   requestAnimationFrame(updateCanvas);
 }
-// this.interval = setInterval(updateCanvas, 20);
+
+this.interval = setInterval(updateCanvas, 20);
 
 document.addEventListener("keydown", (e) => {
   switch (e.code) {
     case "ArrowUp":
       car.moveUp();
-      console.log("up", car);
       break;
     case "ArrowDown":
       car.moveDown();
-      console.log("down", car);
       break;
     case "ArrowRight":
       car.moveRight();
-      console.log("right", car);
       break;
     case "ArrowLeft":
       car.moveLeft();
-      console.log("left", car);
       break;
   }
   updateCanvas();
 });
+
+class Component {
+  constructor(height, width, color, x, y) {
+    this.height = height;
+    this.width = width;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+  }
+  update() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+// const obstacle = new Component(30, 30, "red", 0, 110);
+
+function updateObstacles() {
+  const obstacle = new Component(30, 30, "red", 0, 110);
+  for (i = 0; i < myObstacles.length; i++) {
+    myObstacles[i].y -= -1;
+    myObstacles[i].update();
+  }
+  road.frames += 1;
+  //   console.log(road.frames);
+  if (road.frames % 120 === 0) {
+    console.log(myObstacles);
+    // let x = road.width;
+    // let minHeight = 20;
+    // // let maxHeight = 200;
+    // let height = Math.floor(
+    //   Math.random() * (maxHeight - minHeight + 1) + minHeight
+    // );
+    // let minGap = 50;
+    // let maxGap = 200;
+    // let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+    // myObstacles.push(new Component(50, height, "red", x, 0));
+    // const test = new Component(50, 50, "red", 50, 50);
+    // myObstacles.push(test);
+    // myObstacles.push(new Component(105, 150, "blue", 200, 200));
+    // myObstacles.push(
+    //   new Component(50, x - height - gap, "red", x, height + gap)
+    // );
+  }
+}
